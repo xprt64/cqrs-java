3ae60f90d0c5321d16203bc35b9f98b6df3d59e6
Sorin <sorin@gmail.com>
2020-05-06 15:32:40 +0300

    added some description comment

--- src/main/java/com/cqrs/annotations/CommandHandler.java
+++ src/main/java/com/cqrs/annotations/CommandHandler.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a method of an {@link com.cqrs.base.Aggregate} as being a handler for a command.
+ * A command handler gets the command (a class that implements {@link com.cqrs.base.Command}) as a parameter and emits zero or more events.
+ * The second parameter is optional and is an instance of {@link com.cqrs.commands.CommandMetaData}.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.SOURCE)
 public @interface CommandHandler
